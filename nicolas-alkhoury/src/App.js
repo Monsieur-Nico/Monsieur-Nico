b8741ac320b6d17ece729823418d6bcb4f653cc7
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-25 16:25:48 -0500

    Install React

---
+++ nicolas-alkhoury/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Protfilio">
+      <header className="Protfilio-header">
+        <h1>Nicolas Alkhoury</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
