8759b7ea85f190ef4a7e3c188a943e6ca5e65b96
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-25 20:03:34 -0500

    Add Header component and style

---
+++ nicolas-alkhoury/src/components/Header.jsx
@@ -0,0 +1,10 @@
+// Style
+import "./Styles/Header.scss";
+
+export default function Header(props) {
+  return (
+    <header className="Protfilio-header">
+      <h1 className="header-title">Nicolas Alkhoury</h1>
+    </header>
+  );
+}
