c42c44c31809b790214ce13d5e5a104ae7e3ae39
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-25 22:11:33 -0500

    Fix Header and add NavBar

--- nicolas-alkhoury/src/components/Header.jsx
+++ nicolas-alkhoury/src/components/Header.jsx
@@ -1,10 +1,14 @@
+// Components
+import NavBar from "./NabBar";
+
 // Style
 import "./Styles/Header.scss";
 
 export default function Header(props) {
   return (
     <header className="Protfilio-header">
-      <h1 className="header-title">Nicolas Alkhoury</h1>
+      <h1 className="header-title">Alkhoury, Nicolas</h1>
+      <NavBar />
     </header>
   );
 }
