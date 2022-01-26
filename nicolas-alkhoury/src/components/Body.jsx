13f5dc0eaa1b0569b879d05cf21e4646d1ed08c4
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-01-25 23:15:35 -0500

    Add more details

--- nicolas-alkhoury/src/components/Body.jsx
+++ nicolas-alkhoury/src/components/Body.jsx
@@ -5,18 +5,35 @@ export default function Body(props) {
   return (
     <>
       <section className="body">
-        <div className="body-images">
-          <img
-            className="body-pic"
-            src="https://i.imgur.com/mwf631W.jpg"
-            alt="nicolas"
-          ></img>
+        <div className="body-item">
+          <div className="body-images">
+            <img
+              className="body-pic"
+              src="https://i.imgur.com/mwf631W.jpg"
+              alt="nicolas"
+            ></img>
+          </div>
+          <p className="about-me">
+            Hello! My name is Nicolas Alkhoury. I am a Jr. Full-Stack developer.
+            <br />I live in Ottawa, Ontario. Recent graduate of Web Development
+            bootcamp and in the process of finding a job.
+          </p>
         </div>
-        <p className="about-me">
-          Hello! My name is Nicolas Alkhoury. I am a full-stack developer.
-          <br />I live in Ottawa, Ontario. I am working remotely on some
-          projects.
-        </p>
+        <div className="body-item">
+          <p className="about-me">
+            I am an understanding, patient person with a desire to know, work
+            and communicate with others.
+            <br />I have gained a lot of experience not only in development,
+            also in life, but still... I want more.
+          </p>
+          <div className="body-images">
+            <img
+              className="body-pic"
+              src="https://i.imgur.com/FinJevl.jpg"
+              alt="nicolas"
+            ></img>
+          </div>
+        </div>
       </section>
     </>
   );
