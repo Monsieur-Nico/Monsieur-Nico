7c6fb04b1dc575a20b833984061dc4c6d6946498
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-23 21:45:51 -0500

    Fix Header and NavBar

--- nicolas-alkhoury/src/components/Header.jsx
+++ nicolas-alkhoury/src/components/Header.jsx
@@ -7,7 +7,6 @@ import "./Styles/Header.scss";
 export default function Header(props) {
   return (
     <header className="Protfilio-header">
-      <h1 className="header-title">Alkhoury, Nicolas</h1>
       <NavBar />
     </header>
   );
