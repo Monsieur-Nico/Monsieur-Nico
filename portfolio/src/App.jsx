8273d5bb697de372144f9a2154e86dd283aff877
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-06-24 08:19:07 -0400

    Correct the homepage file

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,24 +1,17 @@
-import logo from "./logo.svg";
+// Components
+import Home from "./Components/Home/index";
+
+// Style
 import "./App.scss";
+
+// Libraries
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Home />
+    </Fragment>
   );
 }
 
---
+++ portfolio/src/Components/Home/index.jsx
@@ -0,0 +1,16 @@
+// Components
+
+// Style
+
+// Libraries
+import { Fragment } from "react";
+
+function Home() {
+  return (
+    <Fragment>
+      <h1>Nico's Portfolio</h1>
+    </Fragment>
+  );
+}
+
+export default Home;
