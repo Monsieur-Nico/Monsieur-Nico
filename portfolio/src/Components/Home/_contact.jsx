7c625a26f39cb15f9614a385f1f72d852583b2c1
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-06-27 19:30:57 -0400

    Create the contact component

---
+++ portfolio/src/Components/Home/_contact.jsx
@@ -0,0 +1,13 @@
+// Components
+import { Fragment } from "react";
+
+// Style
+// Libraries
+
+export default function Contact() {
+  return (
+    <Fragment>
+      <section className="contact"></section>
+    </Fragment>
+  );
+}
