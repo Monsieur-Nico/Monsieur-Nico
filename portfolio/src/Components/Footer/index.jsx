82d0dd55ad206a05e9831e462d32658cfc5cfdcb
Nicolas Alkhoury <74289847+Monsieur-Nico@users.noreply.github.com>
2022-07-20 12:45:25 -0400

    Add links to footer

--- portfolio/src/Components/Footer/index.jsx
+++ portfolio/src/Components/Footer/index.jsx
@@ -8,7 +8,26 @@ import { Fragment } from "react";
 export default function Footer() {
   return (
     <Fragment>
-      <section className="footer"></section>
+      <section className="footer">
+        <a href="https://github.com/Monsieur-Nico" className="github">
+          <ion-icon name="logo-github"></ion-icon>
+        </a>
+        <a href="https://twitter.com/monsieur_nico" className="twitter">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a>
+        <a
+          href="https://discordapp.com/users/718423239071760384"
+          className="discord"
+        >
+          <ion-icon name="logo-discord"></ion-icon>
+        </a>
+        <a
+          href="https://stackoverflow.com/users/16713898/nicolas-alkhoury"
+          className="stackoverflow"
+        >
+          <ion-icon name="logo-stackoverflow"></ion-icon>
+        </a>
+      </section>
     </Fragment>
   );
 }
